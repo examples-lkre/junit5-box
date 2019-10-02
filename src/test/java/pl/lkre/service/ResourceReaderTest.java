5f37196c375a3d0827797ba396fc14799d03d5f5
lkre <lukasz.res@gmail.com>
2019-10-02 19:29:02 +0200

    ResourceReaderTest add exception test

--- src/test/java/pl/lkre/service/ResourceReaderTest.java
+++ src/test/java/pl/lkre/service/ResourceReaderTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import pl.lkre.exception.ResourceReaderException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ResourceReaderTest {
 
@@ -13,5 +14,14 @@ class ResourceReaderTest {
         String actual = resourceReader.fromFile("test.csv");
         String expected = "test;test2;test3";
         assertEquals(expected, actual);
+    }
+
+    @Test
+    void fromFileThrowsException() {
+        ResourceReader resourceReader = new ResourceReader();
+        assertThrows(ResourceReaderException.class,
+                () -> {
+                    resourceReader.fromFile("badFile.csv");
+                });
     }
 }
